8b3c6d2a37ee52b6294e1d737f9c731d570be36d
smile2sm <smile2sm@qq.com>
2020-03-10 22:15:32 +0800

    完善MQ

--- smile2sm-shop-seckill/src/main/java/com/smile2sm/mq/MQConsumer.java
+++ smile2sm-shop-seckill/src/main/java/com/smile2sm/mq/MQConsumer.java
@@ -5,6 +5,7 @@ import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.smile2sm.config.MQConfig;
 import com.smile2sm.config.MqConfig;
 import com.smile2sm.constant.RedisKey;
 
@@ -20,7 +21,7 @@ public class MQConsumer {
 	@Autowired
 	JedisPool jedisPool;
 	
-	@RabbitListener(queues = MqConfig.QUEUE)
+	@RabbitListener(queues = MQConfig.SECKILL_QUEUE)
 	public void receive(String msgBody) {
 		
 		String[] split = msgBody.split(",");
